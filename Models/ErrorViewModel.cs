69b34432922c5021b491fe9dc382acea47fcd947
URL-David <71450523+URL-David@users.noreply.github.com>
2021-05-03 18:15:11 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROYECTO_ED_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
